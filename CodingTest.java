30c2da1c0c753e10ae00ee1609bc8d0dfa2a889c
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-05-02 19:15:18 +0900

    Add files via upload

---
+++ CodingTest.java
@@ -0,0 +1,16 @@
+package hello.demo.practice;
+
+public class CodingTest {
+
+    public static void main(String[] args) {
+        int answer = 0;
+        String my_string = "aAb1B2cC34oOp";
+        String[] strArr = my_string.split("[a-zA-Z]");
+        for(String str: strArr){
+            answer += str.isEmpty() ? 0 : Integer.parseInt(str);
+        }
+        System.out.println(answer);
+
+    }
+
+}
